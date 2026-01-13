5152d8284e480938c7219d65306857c2b138c604
Steeve Morin <steeve@zml.ai>
2026-01-13 11:13:30 +0000

    kvx: fix KVX exception handling

--- llvm/lib/TargetParser/Triple.cpp
+++ llvm/lib/TargetParser/Triple.cpp
@@ -2311,6 +2311,8 @@ ExceptionHandling Triple::getDefaultExceptionHandling() const {
   case Triple::xcore:
   case Triple::xtensa:
     return ExceptionHandling::DwarfCFI;
+  case Triple::kvx:
+    return ExceptionHandling::SjLj;
   default:
     break;
   }
